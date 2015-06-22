ce6d9cbea7385a7f23553d380728da65b150c0b4
Alexander Loshchilov <aloschil@gmail.com>
2015-06-22 18:59:29 +0300

    CHANGE: work circle modification related to vector_field_rendering within snippets

--- snippets/work_circle_matrix_form_sympy.py
+++ snippets/work_circle_matrix_form_sympy.py
@@ -2,7 +2,10 @@ from sympy import Matrix, init_printing, symbols
 from sympy.physics.vector import ReferenceFrame, gradient
 from mayavi import mlab
 
-from vector_field_to_image import vector_field_to_image
+from engine.vector_field_to_image import vector_field_to_image
+from engine.alpha_composite import alpha_composite
+
+#from vector_field_to_image import vector_field_to_image
 from imshow_mayavi import mlab_imshowColor
 from predefined_functions import (get_three_edges_pyramid_expression,
                                   get_four_edges_pyramid_expression,
@@ -51,13 +54,19 @@ P = [Matrix(1, len(p), lambda i, j: 0 if j == k else 1)*((Nu*G).multiply_element
 
 # Let us reduce this potential against weighted against
 # force affect on atoms and number of atoms in specific matter
-#M = [(P[i]*E*Nu[i, :].T)[0] for i in xrange(0, len(p))]
 
 R = ReferenceFrame('R')
 M = [(P[i]*E*Nu[i, :].T)[0].subs(x, R[0]).subs(y, R[1]) for i in xrange(0, len(p))]
-W = [gradient(M[i], R) for i in xrange(len(p))]
+#W = [gradient(M[i], R) for i in xrange(len(p))]
+W = [gradient(M[i], R).to_matrix(R).subs({R[0]: x, R[1]: y})[:2] for i in xrange(len(p))]
 
-im = vector_field_to_image(W[0], R, (-10, 10, 10, -10))
-mlab_imshowColor(im[:, :, :3], im[:, :, -1])
+import parmap
+
+images = parmap.map(vector_field_to_image, W, (-10, 10, 10, -10))
+image = reduce(alpha_composite, images)
+
+im = image # vector_field_to_image(W[0], (-10, 10, 10, -10))
+#im[:, :, -1] = 0
+mlab_imshowColor(im[:, :, :3])
 
 mlab.show()
